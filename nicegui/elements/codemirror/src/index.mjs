c0e550816af8b226546364c518b785948e17bdd2
maybites <registered@maybites.ch>
2026-06-13 20:54:22 +0200

    added autocomplete to codemirror dist

--- nicegui/elements/codemirror/src/index.mjs
+++ nicegui/elements/codemirror/src/index.mjs
@@ -5,4 +5,5 @@ export * from "@codemirror/commands";
 export * from "@codemirror/language";
 export * from "@codemirror/language-data";
 export * from "@codemirror/theme-one-dark";
+export * from "@codemirror/autocomplete";
 export * as themes from "@uiw/codemirror-themes-all";
